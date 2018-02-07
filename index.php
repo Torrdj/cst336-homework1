64e8b35e6ff7ad0b9c1ae157ff1ae153bfda0386
Torrdj <leo.tordjman@epita.fr>
2018-02-07 06:13:17 -0800

    commit all

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>CS - Home</title>
+        
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <h1>Computer Science</h1>
+			<nav>
+				<hr width="50%"/>
+				<a href="index.php" id="current_nav">Home</a>
+				<a href="computers.html">Computers</a>
+				<a href="languages.html">Languages</a>
+				<a href="future.html">Beyond</a>
+			</nav>
+			<hr width="81%"/>
+        </header>
+		
+		<main>
+			<div>
+				<table id="overview">
+					<tr>
+						<td><img src="img/cs_brain.jpg"/></td>
+						<td>
+							<ul><strong><i>Computer Science</i></strong>
+								<li>Computers in a few dates</li>
+								<li>Programming languages</li>
+								<li>To the future and beyond</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2"><center>
+							<em>
+								"Computer Science is no more about computers than astronomy is about telescopes."
+							</em>
+							<br/> - <a href="https://en.wikipedia.org/wiki/Edsger_W._Dijkstra">Edsger W. Dijkstra</a>
+						</center></td>
+					</tr>
+				</table>
+			</div>
+        </main>
+        
+		<footer>
+			<hr width="81%"/>
+            CST 336 - Internet Programming. 2018&copy; TORDJMAN
+			</br></br>
+        </footer>
+    </body>
+</html>
